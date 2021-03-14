e04a9b61aaf23c1b713aeb43dae7f90ba8282486
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2021-03-14 15:58:11 -0700

    ensure admin credentials are parsed from environment

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,9 +2,12 @@ import * as admin from "firebase-admin";
 
 if (admin.apps.length === 0) {
   admin.initializeApp({
-    credential: admin.credential.cert(process.env.NEXT_SERVICE_ACCOUNT as admin.ServiceAccount),
+    credential: admin.credential.cert(
+      JSON.parse(process.env.NEXT_SERVICE_ACCOUNT as string)
+    ),
   });
 }
 
+export const firebaseAdmin = admin;
 export const firestoreAdmin = admin.firestore();
 export const authAdmin = admin.auth();
