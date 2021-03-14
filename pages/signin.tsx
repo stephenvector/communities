f2a53ee8aa99df133a9d55b214b71ec0d1c53e0b
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2021-03-14 16:23:22 -0700

    letting user know if they're already signed in on signin page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Box, Container, SignIn } from "../components";
+import { useAuth } from "../hooks";
 
-const SignUpPage: React.FC = () => (
-  <Container>
-    <Box>
-      <SignIn />
-    </Box>
-  </Container>
-);
+const SignUpPage: React.FC = () => {
+  const { signedIn, currentUser } = useAuth();
+
+  if (signedIn && currentUser) {
+    return (
+      <Container>
+        <Box>Signed in as {currentUser.displayName}</Box>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      <Box>
+        <SignIn />
+      </Box>
+    </Container>
+  );
+};
 
 export default SignUpPage;
